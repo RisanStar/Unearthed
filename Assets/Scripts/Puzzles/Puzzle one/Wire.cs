6a248f71790bf7663ec5630f520edc7c3e2f6ef8
RisanStar <nBuckley2023@elam.co.uk>
2024-04-18 12:44:21 +0100

    -

--- Assets/Scripts/Puzzles/Puzzle one/Wire.cs
+++ Assets/Scripts/Puzzles/Puzzle one/Wire.cs
@@ -1,25 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Wire : MonoBehaviour
 {
+    public SpriteRenderer wireEnd;
+    public GameObject lightOn;
     Vector3 startPoint;
+    Vector3 startPosition;
 
     private void Start()
     {
         startPoint = transform.parent.position;
+        startPosition = transform.position;
     }
 
     private void OnMouseDrag()
     {
-        Vector3 newPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0;
 
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(newPosition, .2f);
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.gameObject != gameObject)
+            {
+                UpdateWire(collider.transform.position);
+                if (transform.parent.name.Equals(collider.gameObject.name))
+                {
+                    collider.GetComponent<Wire>()?.Done();
+                    Done();
+                }
+                return;
+            }
+        }
+        UpdateWire(newPosition);
+    }
+    
+
+    void Done()
+    {
+        lightOn.SetActive(true);
+        Destroy(this);
+    }
+
+    private void OnMouseUp()
+    {
+        UpdateWire(startPosition);
+    }
+
+
+    void UpdateWire(Vector3 newPosition)
+    {
         transform.position = newPosition;
 
         Vector3 direction = newPosition - startPoint;
         transform.right = direction * transform.lossyScale.x;
-    }
 
+        float dist = Vector2.Distance(startPoint, newPosition);
+        wireEnd.size = new Vector2(dist, wireEnd.size.y);
+    }
 }
