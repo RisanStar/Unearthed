16be0050a4de4eb01c6e7b395fb21687b977e182
RisanStar <nBuckley2023@elam.co.uk>
2024-04-24 11:53:30 +0100

    -

--- Assets/Scripts/NPCs/Dialogue.cs
+++ Assets/Scripts/NPCs/Dialogue.cs
@@ -1,13 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Ink.Runtime;
 
-
-[System.Serializable]
-public class Dialogue 
+public class Dialogue : MonoBehaviour
 {
-    public string name;
+   private static Dialogue instance;
 
-    [TextArea(3, 10)]
-    public string[] sentences;
+    [SerializeField] private GameObject dialogueUI;
+    [SerializeField] private TextMeshProUGUI dialogueText;
+
+    private Story currentStory;
+
+    private bool dialogueIsPlaying;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Found More Than One Dialogue In The Scene");
+        }
+        instance = this;
+    }
+
+    public static Dialogue GetInstance()
+    {
+        return instance;
+    }
+
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialogueUI.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(!dialogueIsPlaying)
+        {
+            return; 
+        }
+
+        if (Input.GetButtonDown("E"))
+        {
+            ContinueStory();
+        }
+    }
+
+    public void EnterDialogueMode(TextAsset ink)
+    {
+        currentStory = new Story(ink.text);
+        dialogueIsPlaying = true;
+        dialogueUI.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            dialogueText.text = currentStory.Continue();
+        }
+        else
+        {
+            ExitDialogueMode();
+        }
+    }
+
+    private void ExitDialogueMode()
+    {
+        dialogueIsPlaying = false;
+        dialogueUI.SetActive(false);
+        dialogueText.text = "";
+    }
 }
